681d9415c9a2bc4702f110a18d989d6721de75b8
doubleF <lbq0335021@gmail.com>
2025-01-02 17:01:33 +0800

    optimize

--- code/push.js
+++ code/push.js
@@ -1,7 +1,31 @@
 const axios = require("axios");
-
+// pic id write into picUUID.json
+async function writeIntoFile(picArr) {
+  const jsonFilePath = path.join(__dirname, 'picUUID.json');
+  fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('读取文件失败:', err);
+      return;
+    }
+    try {
+      const jsonData = JSON.parse(data);
+      // 假设JSON结构中有一个数组，向数组中追加数据
+      if (Array.isArray(jsonData)) {
+        jsonData.push(picArr);
+      }
+      // 将更新后的JSON数据写回文件
+      fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2), (writeErr) => {
+        if (writeErr) {
+          console.error('写入文件失败:', writeErr);
+        }
+      });
+    } catch (parseErr) {
+      console.error('解析JSON失败:', parseErr);
+    }
+  });
+};
 // 封装请求函数
-async function sendPostRequest(data) {
+async function sendPostRequest(paperNo, data, idArr) {
   if(!data["year"]) data["year"] = "";
   if(!data["category"]) data["category"] = "";
   if(!data["question_type"]) data["question_type"] = "";
@@ -15,9 +39,14 @@ async function sendPostRequest(data) {
   if(!data["grade"]) data["grade"] = "";
   if(!data["question_context"]) data["question_context"] = "";
   if(!data["marks"]) data["marks"] = "";
-  if("pic" in data) delete data["pic"];
+  if("pic" in data) {
+    if (data["pic"] === "1") {
+      data["question"] = `#${data["no"]}@${paperNo}《${data["question"]}`;
+    }
+    delete data["pic"];
+  }
   if("no" in data) delete data["no"];
-  console.log(`data: ${JSON.stringify(data)}`);
+  // console.log(`data: ${JSON.stringify(data)}`);
   const url = "https://www.hktrinityedu.com/service/saveaidata.ke";
   try {
     const response = await axios.post(url,
@@ -29,15 +58,28 @@ async function sendPostRequest(data) {
         }
       }
     );
-    console.log("Response:", response.data);
+    if (data["question"].startsWith("#")) {
+      console.log("pic Response:", response.data);
+      idArr.push(response.data);
+    }
+    // else console.log("Response:", response.data);
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
-const dataSet = [];
-
-// 遍历 dataSet 并发送请求
-dataSet.forEach(data => {
-  sendPostRequest(data);
-});
+const fs = require('fs');
+const path = require('path');
+const fileName = '19';
+const jsonPath = path.join(__dirname, '../paper/simulation/economic/', fileName + '-1.json');
+const dataSet = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
+async function func() {
+  let idArr = [];
+  // 遍历 dataSet 并发送请求
+  for (const data of dataSet) {
+    await sendPostRequest(fileName, data, idArr);
+  }
+  console.log('idArr size: ', idArr.length);
+  writeIntoFile(idArr);
+};
+func();
